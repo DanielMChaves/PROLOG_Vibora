9db618d7a5d22343d29458fee5f3fb541f2ddd22
DanielMChaves <daniel.melero.95@gmail.com>
2017-04-09 12:25:24 +0200

    Enunciado

---
+++ vibora_lista_aninada.pl
@@ -0,0 +1,12 @@
+:- module(_,_).
+
+% Modos de Uso
+
+% Pseudocodigo
+
+% vibora/4(colores, lista_m, lista_n, resultado)
+vibora(C, TM, TN, R).
+
+% Ejemplo
+% ?- vibora([a,b,c,d],[_,_,_,_,_],[_,_,_],R).
+% R = [[a,b,c,d,a],[b,a,d,c,b],[c,d,a,b,c]] ? 
